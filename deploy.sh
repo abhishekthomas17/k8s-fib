6db8bcaa90108cf74677a87cba6f9942e54d0981
abhishek-thomas-hv <abhishek.thomas@hyperverge.co>
2021-09-12 19:29:13 +0530

    modified deply.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-docker build --no-cache -t abhitom17/docker-fib-client t abhitom17/docker-fib-client:$GIT_SHA ./client
-docker build --no-cache -t abhitom17/docker-fib-server t abhitom17/docker-fib-server:$GIT_SHA ./server
-docker build --no-cache -t abhitom17/docker-fib-worker t abhitom17/docker-fib-worker:$GIT_SHA ./worker
+docker build --no-cache -t abhitom17/docker-fib-client -t abhitom17/docker-fib-client:$GIT_SHA ./client
+docker build --no-cache -t abhitom17/docker-fib-server -t abhitom17/docker-fib-server:$GIT_SHA ./server
+docker build --no-cache -t abhitom17/docker-fib-worker -t abhitom17/docker-fib-worker:$GIT_SHA ./worker
 
 docker push abhitom17/docker-fib-client
 docker push abhitom17/docker-fib-client:$GIT_SHA
@@ -13,6 +13,6 @@ docker push abhitom17/docker-fib-worker:$GIT_SHA
 
 kubectl apply -f k8s
 
-kubectl set images deployments/client-deployment client=abhitom17/docker-fib-client:$GIT_SHA
-kubectl set images deployments/server-deployment server=abhitom17/docker-fib-server:$GIT_SHA
-kubectl set images deployments/worker-deployment worker=abhitom17/docker-fib-worker:$GIT_SHA
+kubectl set image deployments/client-deployment client=abhitom17/docker-fib-client:$GIT_SHA
+kubectl set image deployments/server-deployment server=abhitom17/docker-fib-server:$GIT_SHA
+kubectl set image deployments/worker-deployment worker=abhitom17/docker-fib-worker:$GIT_SHA
